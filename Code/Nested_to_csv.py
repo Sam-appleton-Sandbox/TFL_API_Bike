f74965962d71eb61c512538646df83a981e3a0f4
Sam-appleton-Sandbox <samuel.appleton.uk@gmail.com>
2023-04-21 20:24:27 +0100

    Added OS detector

--- Code/Nested_to_csv.py
+++ Code/Nested_to_csv.py
@@ -4,22 +4,34 @@ import numpy as np
 import datetime
 import arrow
 import time
+from sys import platform
 
 
 
+base_path_win ='C:/Users/samue/OneDrive/Documents/TFL_API_Bike/Code/Outputs/'
+base_path_apple = '/Users/samappleton/Documents/TFL_API_Bike/Code/Outputs/'
 
-
-api_path = "C:/Users/samue/OneDrive/Documents/TFL_API_Bike/Code/Outputs/API.csv"
-api_vehicle_path ="C:/Users/samue/OneDrive/Documents/TFL_API_Bike/Code/Outputs/API_Vehicle.csv"
-api_casualty_path = "C:/Users/samue/OneDrive/Documents/TFL_API_Bike/Code/Outputs/API_Casualty.csv"
 
 timestr = datetime.datetime.now().strftime("%Y-%m-%d")
+
 
 
 def new(output_data):
     print("PROCESS 'NESTED_TO_CSV.PY' HAS STARTED RUNNING----------")
     start_time1 = time.time()   
 
+    if platform == "linux" or platform == "linux2":
+        OS = 'linux'
+    elif platform == "darwin":
+        print('Apple')
+        api_path = f"{base_path_apple}API.csv"
+        api_vehicle_path =f"{base_path_apple}API_Vehicle.csv"
+        api_casualty_path = f"{base_path_apple}API_Casualty.csv"
+    elif platform == "win32":
+        print('Windows')
+        api_path = f"{base_path_win}API.csv"
+        api_vehicle_path =f"{base_path_win}API_Vehicle.csv"
+        api_casualty_path = f"{base_path_win}API_Casualty.csv" 
 
     def write_file(file,output):
         file.to_csv(output)
@@ -74,9 +86,9 @@ def new(output_data):
 
             
         
-        write_file(api,"C:/Users/samue/OneDrive/Documents/TFL_API_Bike/Code/Outputs/API.csv")
-        write_file(df_vehicle,"C:/Users/samue/OneDrive/Documents/TFL_API_Bike/Code/Outputs/API_Vehicle.csv")
-        write_file(df_cas,"C:/Users/samue/OneDrive/Documents/TFL_API_Bike/Code/Outputs/API_Casualty.csv")
+        write_file(api,api_path)
+        write_file(df_vehicle,api_vehicle_path)
+        write_file(df_cas,api_casualty_path)
 
 
         
--- Code/Test.py
+++ Code/Test.py
@@ -1,10 +1,14 @@
-from sys import platform
+# from sys import platform
 
-if platform == "linux" or platform == "linux2":
-    OS = 'linux'
-elif platform == "darwin":
-    OS =  'X'
-elif platform == "win32":
-    OS = 'Windows'
+# if platform == "linux" or platform == "linux2":
+#     OS = 'linux'
+# elif platform == "darwin":
+#     OS =  'X'
+# elif platform == "win32":
+#     OS = 'Windows'
     
-print(OS)
+# print(OS)
+
+one = 'one'#
+two = f"two {one}"
+print(two)
