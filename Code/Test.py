b41ff5a536200a88581b432740d6818f09a33162
Sam Appleton <samuel.appleton.uk@gmail.com>
2023-04-28 21:42:35 +0100

    commiting

--- Code/Test.py
+++ Code/Test.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # from sys import platform
 
 # if platform == "linux" or platform == "linux2":
@@ -12,3 +13,17 @@
 one = 'one'#
 two = f"two {one}"
 print(two)
+=======
+from sys import platform
+
+# if platform == "linux" or platform == "linux2":
+#     OS = 'linux'
+# elif platform == "darwin":
+#     OS =  'X'
+# elif platform == "win32":
+#     OS = 'Windows'
+    
+# print(OS)
+
+print(platform)
+>>>>>>> 4fdfad6 (commiting)
